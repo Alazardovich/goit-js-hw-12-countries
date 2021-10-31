148aff253bd4ee61930f3367b4dbcd4f99382893
Artem <lazarazuy@gmail.com>
2021-10-31 17:19:04 +0100

    so some add

--- src/js/padination.js
+++ src/js/padination.js
@@ -1,5 +1,8 @@
 import API from './fetchCountries.js'
 import tamplate from "../template/template.hbs"
+import { alert, notice, info, success, error, defaultModules } from '@pnotify/core';
+import "@pnotify/core/dist/PNotify.css";
+import "@pnotify/core/dist/BrightTheme.css";
 
 const debounce = require('lodash.debounce');
 
@@ -14,11 +17,40 @@ function onSearchCountry(event) {
     const searchQuery = form.elements.query.value;
     API.fetchCountries(searchQuery)
     .then(renderCountriesTamplate)
-    .catch(err =>console.log(err)
-    .finally(() => form.reset))
+    .catch(err =>console.log(err))
+    // .finally(() => form.reset))
 }
 function renderCountriesTamplate(country) {
-    const markup = tamplate(country);
-    container.insertAdjacentHTML('beforeend', markup)
     
+    if (!country.length) {
+        error({
+            title: 'Incorrect request!',
+            text: 'Check if the country name is entered correctly',
+            delay: 2000,
+                });
+                return clearInput();
+    }
+    else if (country.length >= 10) {
+        error({
+            title: 'Incorrect request!',
+            text: 'Too many matches found. Please enter a more specific query!',
+            delay: 2000,
+                });
+                return clearInput();
+    }
+    else if (country.length >=2 && country.length < 10) {
+        const listName = country
+      .map(el => {
+        return `<li><h1>${el.name}</h1></li>`;
+      })
+      .join('');
+        return container.insertAdjacentHTML('beforeend', listName);
+    }
+    else if (country.length < 2) {
+        const markup = tamplate(country);
+        container.insertAdjacentHTML('beforeend', markup) 
+    }
+}
+function clearInput(){
+    container.innerHTML = '';
 }
